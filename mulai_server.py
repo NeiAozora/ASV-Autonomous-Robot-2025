bb309fe2520b2291a433b85077e9335c0adbd32a
NeiAozora <noxindocraft@gmail.com>
2025-10-12 02:20:53 +0700

    update

--- mulai_server.py
+++ mulai_server.py
@@ -3,6 +3,8 @@ from helpers.parallel_runner import ParallelRunner
 
 p = ParallelRunner()
 
-p.add_process("camera_server", "")
-p.add_process("camera_server", "")
+p.add_process("camera_server", "bash server/camera/camera_v2_go/run_server.sh")
+p.add_process("controller_server", "cd server/control && uvicorn server:app --host 0.0.0.0 --port 2000 --workers 4")
 p.add_process("server_tranceiver", "go run server/tranceiver/main.go", True, None)
+
+p.run()
